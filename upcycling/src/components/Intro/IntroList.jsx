05c14bd9dc17875a7d241bb1f3f0a70e5cb9737a
rachiyuhu <kmoukhj0126@gmail.com>
2022-06-17 15:52:01 +0900

    0617

--- upcycling/src/components/Intro/IntroList.jsx
+++ upcycling/src/components/Intro/IntroList.jsx
@@ -6,6 +6,7 @@ import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 gsap.registerPlugin(ScrollTrigger);
 
+
 function animateFrom(elem, direction) {
         direction = direction || 1;
         var x = 0,
@@ -47,62 +48,138 @@ const IntroList = () => {
         });
 
     return (
-        <div>
-            <Nav/>
-            <div className="cInnerContent">
-                <h1 className="header-section gs_reveal ipsType_center">Upcycling</h1>
-                    <div className="features">
-                        <div className="feature ipsSpacer_bottom_double ipsGrid ipsGrid_collapsePhone">
-                            <div className="featured-image-container ipsGrid_span5 gs_reveal gs_reveal_fromLeft">
-                                <div className="card">
-                                    <img width="479" src="../../images/Category003.jpg" alt="img1"/>
-                                </div>
-                            </div>
-                            <div className="ipsGrid_span7 ipsType_left">
-                                <h2 className="heading_large gs_reveal">For <strong>Upcycling</strong></h2>
-                                <p className="gs_reveal">새활용(-活用) 또는 업사이클(영어: upcycling 또는 creative reuse)은 부산물, 폐자재와 같은 쓸모 없거나 버려지는 물건을 새롭게 디자인해 예술적·환경적 가치가 높은 물건으로 재탄생시키는 재활용 방식이다. </p>
-                            </div>
-                        </div>
-                    <div className="feature ipsSpacer_bottom_double ipsGrid ipsGrid_collapsePhone">
-                        <div className="ipsGrid_span7 ipsType_right">
-                            <h2 className="heading_large gs_reveal"> 예술 </h2>
-                            <p className="gs_reveal"> 주류 미술에서 발견된 물건(objet trové)을 재사용하는 전통은 민속 예술에서 오랫동안 사용된 방식이기는 했지만, 20세기 성숙기를 겪으며 더욱 발전했다. 예를 들어 아미쉬 퀼트는 인양된 천을 재사용한 것이다. 로스앤젤레스에 있는 사이먼 로디아의 와츠 타워(1921–1954)는 고철, 도자기, 깨진 유리의 거대한 재활용의 대표적 예이며, 이는 와츠 스카이라인에서 30미터가 넘는 가장 높은 17개의 구조물로 구성되어 있다.</p>
-                        </div>
-                        <div className="featured-image-container ipsGrid_span5 gs_reveal gs_reveal_fromRight">
-                            <div className="card">
-                                <img width="479" src="../../images/222.PNG" alt="img2"/>
-                            </div>
-                        </div>
+    <div>
+        <Nav/>
+        <div className="cInnerContent">
+            {/*1단 나누기 */}
+            <div className="cInnerContent_1">
+                    <div className="cInnerContent_1_left">
+                        <h1>다시 한 번, <br /></h1> 
+                        <h3>지구를 되살리기 위해</h3>
+                        <p>
+                            대한민국에서 버려지는 생수 페트병은 연간 49억개.<br />
+                            다 쓴 페트병을 모두 연결하면 지구를 10바퀴도 넘게 돌 수 있는 양입니다.<br />
+                            <br />
+                            전세계 1인당 일회용 플라스틱 폐기물 배출량 3위를 기록한 
+                            우리나라가 할 수 있는 일은 과연 무엇이었을까요?<br />
+                            <br /></p> 
+                            
+                            분류 배출을 넘어 쓰레기를 새로운 방법으로 재탄생시키는
+                            <p><b className='point'>“Upcycling”</b><br />
+                            저희는 이 단어로부터 출발했습니다.
+                            </p>
                     </div>
+                    <div className="cInnerContent_1_right">
+                        <img className="intro01"src="../../images/intro01(수정).PNG"></img>
+                    </div>
+            </div>
+             {/*2단 나누기 */}
+            <div className="cInnerContent_2">
+                <div className="cInnerContent_2header">
+                    <h1>Upcycling?</h1>
+                </div>
                     <div className="feature ipsSpacer_bottom_double ipsGrid ipsGrid_collapsePhone">
                         <div className="featured-image-container ipsGrid_span5 gs_reveal gs_reveal_fromLeft">
                             <div className="card">
-                                <img width="479" src="../../images/산업.jpg" alt="img3"/>
+                                <img width="479" src="../../images/intro02.JPG" alt="img2"/>
                             </div>
                         </div>
                         <div className="ipsGrid_span7 ipsType_left">
-                            <h2 className="heading_large gs_reveal">산업 </h2>
-                            <p className="gs_reveal"> 플라스틱 및 전자 제조와 같은 많은 산업 공정은 유한한 자원의 소비에 의존한다.또한 폐기물은 환경에 악영향을 미칠 수 있으며 인간의 건강에 영향을 미칠 수 있다. 이러한 맥락에서 새활용은 폐기물 또는 부산물에서 더 높은 가치를 지닌 제품을 만들어 폐기물과 자원 소비를 줄이기 위해 사용 가능한 미래 기술의 사용을 설명한다.
-                            소비자 가전에서 중고 제품의 재 제조 또는 리퍼브 프로세스는 새로운 제조에 비해 에너지 및 재료 소비가 감소하기 때문에 업 사이클링으로 볼 수 있다. 재생산된 제품은 폐기하거나 재활용하는 것보다 가치가 더 높다. 양조 공정에서 나오는 폐기물을 바이오가스 공정의 기판으로 사용함으로써 폐기할 필요가 없어지고 전체 양조 공정에 상당한 이익을 창출할 수 있다. 기판 가격에 따라 운영비의 약 20%의 이익이 가능하다. 이 과정에서 바이오가스 공장은 "업사이클러" 역할을 한다.</p>
+                            <h2 className="heading_large gs_reveal">1. 업사이클링이란</h2>
+                            <p className="gs_reveal">
+                                업사이클링이란 쓸모를 다한 물건의 부산물, 폐자재와 같은 재활용품에 디자인
+                                또는 활용도를 더해 예술적·환경적 가치가 높은 물건으로 재탄생시키는 방식을 말합니다.
+                            </p> 
                         </div>
                     </div>
-                    <div className="feature ipsSpacer_bottom_double ipsGrid ipsGrid_collapsePhone">
-                        <div className="ipsGrid_span7 ipsType_right">
-                            <h2 className="heading_large gs_reveal">의류</h2> 
-                            <p className="gs_reveal">디자이너들은 산업 섬유 폐기물과 기존 의류를 모두 새로운 패션을 만드는 기본 재료로 사용하기 시작했다. 업사이클링은 소비 이전 또는 소비 이후 폐기물을 사용하거나 둘의 조합을 사용하는 것으로 알려져 있다. 소비 이전 폐기물은 공장에서 패턴을 도려내고 남은 원단 잔해와 같이 만들어진다. 소비 이후 폐기물은 기증받은 옷과 같이 주인에게 더 이상 유용하지 않을 때 완성된 제품을 말한다.
-                            종종, 사람들은 사고, 사용하고, 버리는 것에 만족하는 선형 경제를 실행한다. 이 시스템은 수백만 킬로그램의 섬유 폐기물이 버려지는 데 기여한다. 생산되는 대부분의 섬유는 재활용이 가능하지만, 미국에서만 약 85%가 토지 매각으로 귀결된다. 지속 가능한 삶을 살기 위해서는 패스트패션에 의해 장려되는 "버리는" 태도와는 반대되는 의복 선택이 필요하다. 새활용(upcycling)은 이것을 도와 줄 수 있는데, 그것은 그것이 더 순환적인 경제 모델을 실행에 옮기기 때문이다. 
-                            순환 경제(Circular Economy)는 리소스를 최대한 오래 사용하여 사용 중에 최대한 많은 가치를 창출한 다음, 사용이 끝나면 복원하고 용도 변경하는 것을 말한다.
-                            맥도너와 브라웅가트에 의해 대중화된 이것은 요람 대 요람 원리로도 알려져 있다. 이 원리는 제품이 여러 수명 주기를 가지거나 생분해되도록 설계되어야 한다고 말한다.</p>
-                        </div>
-                        <div className="featured-image-container ipsGrid_span5 gs_reveal gs_reveal_fromRight">
-                            <div className="card">
-                                <img width="479" src="../../images/의류.jpg" alt="img4"/>
-                            </div>
+                <div className="feature ipsSpacer_bottom_double ipsGrid ipsGrid_collapsePhone">
+                    <div className="ipsGrid_span7 ipsType_right">
+                        <h2 className="heading_large gs_reveal">2. 예술</h2>
+                        <p className="gs_reveal"> 
+                        물건을 재사용하는 전통은 민속 예술부터 오랫동안 사용되어 왔습니다. 
+                        대표적인 예로 아미쉬 퀼트는 인양된 천을 재사용하여 제작한 작품이며, 
+                        로스앤젤레스에 있는 이먼 로디아의 와츠 타워(1921–1954)는 고철, 도자기, 깨진 유리를 재활용한 대표적인 작품으로 알려져 있습니다. 
+                        </p>
+                    </div>
+                    <div className="featured-image-container ipsGrid_span5 gs_reveal gs_reveal_fromRight">
+                        <div className="card">
+                            <img width="479" src="../../images/intro03.PNG" alt="img3"/>
                         </div>
                     </div>
                 </div>
+                <div className="feature ipsSpacer_bottom_double ipsGrid ipsGrid_collapsePhone">
+                    <div className="featured-image-container ipsGrid_span5 gs_reveal gs_reveal_fromLeft">
+                        <div className="card">
+                            <img width="479" src="../../images/intro04.jpg" alt="img4"/>
+                        </div>
+                    </div>
+                    <div className="ipsGrid_span7 ipsType_left">
+                        <h2 className="heading_large gs_reveal">3. 산업 </h2>
+                        <p className="gs_reveal"> 
+                        중고 제품의 재제조, 혹은 리퍼브로 만들어진 재생산된 제품은 폐기하거나 재활용하는 것보다 더 높은 가치를 가집니다. 
+                        양조 공정에서 나오는 폐기물을 바이오가스 공정의 기판으로 사용함으로써 폐기할 필요가 없어지고 전체 양조 공정에 상당한 이익을 창출할 수 있어, 
+                        이 과정에서 바이오가스 공장은 "업사이클러" 역할을 한다고 볼 수 있습니다.
+                        </p>
+                    </div>
+                </div>
+                <div className="feature ipsSpacer_bottom_double ipsGrid ipsGrid_collapsePhone">
+                    <div className="ipsGrid_span7 ipsType_right">
+                        <h2 className="heading_large gs_reveal">4. 의류</h2> 
+                        <p className="gs_reveal">
+                        현대 디자이너들은 산업 섬유 폐기물과 기존 의류를 모두 새로운 패션을 만드는 기본 재료로 사용하기 시작했습니다. 
+                        산업 섬유 폐기물이란 공장에서 패턴을 도려내고 남은 원단 잔해와 같이 만들어진 것을 의미하며, 
+                        기증받은 옷 등 주인에게 더이상 유용하지 않은, 완성된 제품 역시 소비 이후 폐기물이라 일컬어집니다. 
+                        <br />
+                        이 때 순환 경제(Circular Economy)란 리소스를 최대한 오래 사용하여 사용 중에 최대한 많은 가치를 창출한 다음, 
+                        사용이 끝나면 복원하거나 용도를 변경하는 것을 말합니다. 
+                        섬유 폐기물 및 기존 의복을 재활용 하는 것 역시 업사이클링의 한 종류로 볼 수 있습니다.
+                        </p>
+                    </div>
+                    <div className="featured-image-container ipsGrid_span5 gs_reveal gs_reveal_fromRight">
+                        <div className="card">
+                            <img width="479" src="../../images/intro05.jpg" alt="img5"/>
+                        </div>
+                    </div>
+                </div>
+             
             </div>
+
+            {/*3단 나누기*/}
+                <div className='cInnerContent_3'>
+                    <h1>다시에서 우리는 아래와 같은 활동을 합니다.</h1>
+                <ul className='cInnerContent_31'>
+                    <li className='cInnerContent_list'>
+                    <i class="fab fa-envira"></i>
+                        <h3>아껴쓰기</h3>
+                        <p>내용1</p>
+                    </li>
+                    <li className='cInnerContent_list'>
+                    <i class="fas fa-oil-can"></i>
+                        <h3>바꿔쓰기</h3>
+                        <p>내용1</p>
+                    </li>
+                    <li className='cInnerContent_list'>
+                    <i class="fas fa-exchange-alt"></i>
+                        <h3>나눠쓰기</h3>
+                        <p>내용1</p>
+                    </li>
+                    <li className='cInnerContent_list'>
+                    <i class="fas fa-recycle"></i>
+                        <h3>다시쓰기</h3>
+                        <p>내용1</p>
+                    </li>
+                </ul>
+            </div>
+            
+            {/*4단 나누기*/}
+            <div className='cInnerContent_4'>
+                <h1>Review Community</h1>
+                {/*그리드 3*3단으로 Review 게시물 가져오기*/}
+
+            </div>
+            {/*인트로 종료 */}
         </div>
+    </div>
     );
 };
 export default IntroList;
