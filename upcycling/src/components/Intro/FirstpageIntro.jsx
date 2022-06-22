f9315e69695a6d4c6abfd10e17fbdb5e754e687b
junwookim <ekdlskalrx98@naver.com>
2022-06-22 17:55:47 +0900

    firlog

--- upcycling/src/components/Intro/FirstpageIntro.jsx
+++ upcycling/src/components/Intro/FirstpageIntro.jsx
@@ -8,7 +8,7 @@ return (
             <h1>UPCYCLE TOWN</h1>
             <p>업사이클링 아이디어와 영감</p>
         </div>
-        <div className='first_page_secondbox'>
+        <div className='first_page_firstbox'>
             <img className='first_intro_img' src="../../images/frontImg3.jpg"></img>
             <div className='first_intro_card'>
                 <h2>What Is Upcycling?</h2>
@@ -17,18 +17,18 @@ return (
                 <p>그래서 아이템의 가치가 높아졌다고 해서 업사이클링이라고 해요!</p>
             </div>
         </div>
-        <div className='first_page_thirdbox'>
-            <img className='first_intro_img' src="../../images/frontImg3.jpg"></img>
-            <div className='first_intro_card'>
+        <div className='first_page_secondbox'>
+            <img className='second_intro_img' src="../../images/frontImg3.jpg"></img>
+            <div className='second_intro_card'>
                 <h2>What 22222?</h2>
                 <p>업사이클링은 낭비라고 생각되는 것을 재활용하는 것입니다.</p>
                 <p>업사이클링된 아이템은 종종 이전보다 더 기능적이거나 아름다워집니다.</p>  
                 <p>그래서 아이템의 가치가 높아졌다고 해서 업사이클링이라고 해요!</p>
             </div>
         </div>
-        <div className='first_page_fourthbox'>
-            <img className='first_intro_img' src="../../images/frontImg3.jpg"></img>
-            <div className='first_intro_card'>
+        <div className='first_page_thirdbox'>
+            <img className='third_intro_img' src="../../images/frontImg3.jpg"></img>
+            <div className='third_intro_card'>
                 <h2>What 3333?</h2>
                 <p>업사이클링은 낭비라고 생각되는 것을 재활용하는 것입니다.</p>
                 <p>업사이클링된 아이템은 종종 이전보다 더 기능적이거나 아름다워집니다.</p>  
