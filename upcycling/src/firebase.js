032fe9e6ef2caf3df64a898ec5ee2e71ae268663
junwookim <87688835+junwookim0@users.noreply.github.com>
2022-06-23 16:35:01 +0900

    Update firebase.js

--- upcycling/src/firebase.js
+++ upcycling/src/firebase.js
@@ -7,15 +7,15 @@ import { GoogleAuthProvider, signInWithPopup,
 } from 'firebase/auth';
 import { getStorage } from "firebase/storage";
 const firebaseConfig = {
-    
+    /*
     //파이어베이스 인증키랑 지울게요! -지은-
-    apiKey: "AIzaSyBoX4JqMqLdCoJM7g0EiZor0VcdiqTcDwo",
-    authDomain: "fir-test-login-85c6c.firebaseapp.com",
-    projectId: "fir-test-login-85c6c",
-    storageBucket: "fir-test-login-85c6c.appspot.com",
-    messagingSenderId: "366758576094",
-    appId: "1:366758576094:web:ed6d85b8de98363efa674f",
-    
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    */
 };
 // Initialize Firebase 
 
