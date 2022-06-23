f77a823bcf9f59359889d375f46ca852332644c5
junwookim <ekdlskalrx98@naver.com>
2022-06-23 10:14:42 +0900

    github login

--- upcycling/src/firebase.js
+++ upcycling/src/firebase.js
@@ -7,9 +7,7 @@ import { GoogleAuthProvider, signInWithPopup,
 } from 'firebase/auth';
 import { getStorage } from "firebase/storage";
 const firebaseConfig = {
-
-
-
+    /* 
     //파이어베이스 인증키랑 지울게요! -지은-
 
     apiKey: "",
@@ -19,8 +17,6 @@ const firebaseConfig = {
     messagingSenderId: "",
     appId: ""
     */
-   
-
 
 };
 // Initialize Firebase 
