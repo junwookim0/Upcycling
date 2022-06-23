db7324727f79d8a9ad3cba46f4c2eefb352f20ef
junwookim0 <87688835+junwookim0@users.noreply.github.com>
2022-06-23 11:29:36 +0900

    Update firebase.js

--- upcycling/src/firebase.js
+++ upcycling/src/firebase.js
@@ -17,12 +17,6 @@ const firebaseConfig = {
     messagingSenderId: "",
     appId: ""
     */
-    apiKey: "AIzaSyBoX4JqMqLdCoJM7g0EiZor0VcdiqTcDwo",
-    authDomain: "fir-test-login-85c6c.firebaseapp.com",
-    projectId: "fir-test-login-85c6c",
-    storageBucket: "fir-test-login-85c6c.appspot.com",
-    messagingSenderId: "366758576094",
-    appId: "1:366758576094:web:ed6d85b8de98363efa674f"
 };
 // Initialize Firebase 
 
