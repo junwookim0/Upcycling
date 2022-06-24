619eb21d14760340670031e7a600174fb874dd6d
junwookim <87688835+junwookim0@users.noreply.github.com>
2022-06-24 17:35:14 +0900

    Update firebase.js

--- upcycling/src/firebase.js
+++ upcycling/src/firebase.js
@@ -12,12 +12,7 @@ const firebaseConfig = {
     /*
     
     */
-    apiKey: "AIzaSyBoX4JqMqLdCoJM7g0EiZor0VcdiqTcDwo",
-    authDomain: "fir-test-login-85c6c.firebaseapp.com",
-    projectId: "fir-test-login-85c6c",
-    storageBucket: "fir-test-login-85c6c.appspot.com",
-    messagingSenderId: "366758576094",
-    appId: "1:366758576094:web:ed6d85b8de98363efa674f"
+    
 };
 // Initialize Firebase 
 
