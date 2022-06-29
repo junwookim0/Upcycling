6975b7003e99448290993f8c9f566250ef6f95b1
junwookim <ekdlskalrx98@naver.com>
2022-06-29 13:57:42 +0900

    0629 13:57

--- upcycling/src/page/FirstMain/FirstMain.jsx
+++ upcycling/src/page/FirstMain/FirstMain.jsx
@@ -14,18 +14,14 @@ const FirstMain = () => {
     return (
         <div>
             <Login/>
-            <div className='video_box_main'>
-                <video autoPlay muted loop className="introVideo_main">
-                    <source src="../../video/file.mp4" type="video/mp4"/>
-                </video>
-                <div className="signupBtn_div">
-                    <button className="signupBtn" onClick={SignUp}>시작하기 Click!</button>
-                </div>
-                <div className='video_text_main'>
-                    <h2 className='video_text_main_h2'>업사이클링(up-cycling)</h2>
-                    <h3 className='video_text_main_h3'>업사이클링을 실천하는 여러분의 이야기를 들려주세요!</h3>
-                    <button onClick={SignUp}>시작하기 Click!</button>
-                </div>
+            <div className='first_page_intro_div'>
+                <h1 className='first_page_intro_h1'>업사이클링(up-cycling)</h1>
+                <h2>업타운에 오신 여러분을 환영합니다</h2>
+                <h3 className='first_page_intro_h3'>
+                    업타운은 업사이클링을 알리기 위한 커뮤니티 입니다 <br/>
+                    여러분의 업사이클링 경험과 아이디어를 공유하고<br/>
+                    마켓을 통해 다양한 업사이클링 제품을 만나보세요.    
+                </h3>
             </div>
             <FirstpageIntro/>
         </div>
