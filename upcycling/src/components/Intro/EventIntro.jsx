438d03e1dfc6ee805227e5684f435b5d78b4ce13
junwookim <ekdlskalrx98@naver.com>
2022-06-30 09:50:07 +0900

    0630 9:50 김준우

--- upcycling/src/components/Intro/EventIntro.jsx
+++ upcycling/src/components/Intro/EventIntro.jsx
@@ -6,43 +6,44 @@ import "./EventIntro.css";
 
 const EventIntro = () => {
     const IMAGES =
-    [{
-        src: "../../images/event1.PNG",
-        thumbnail: "../../images/event1.PNG",
-    },
-    {
-        src: "../../images/event2.PNG",
-        thumbnail: "../../images/event2.PNG",
-    },
-    {
-        src: "../../images/event3.PNG",
-        thumbnail: "../../images/event3.PNG",
-    },
-    {
-        src: "../../images/event4.PNG",
-        thumbnail: "../../images/event4.PNG",
-    },
-    {
-        src: "../../images/event5.jpg",
-        thumbnail: "../../images/event5.jpg",
-    },
-    {
-        src: "../../images/event6.jpg",
-        thumbnail: "../../images/event6.jpg",
-    },
-    {
-        src: "../../images/event7.jpg",
-        thumbnail: "../../images/event7.jpg",
-    },
-    {
-        src: "../../images/event8.jpg",
-        thumbnail: "../../images/event8.jpg",
-    },
-    {
-        src: "../../images/event9.jpg",
-        thumbnail: "../../images/event9.jpg",
-    },
-]
+    [
+        {
+            src: "../../images/event4.PNG",
+            thumbnail: "../../images/event4.PNG",
+        },
+        {
+            src: "../../images/event5.jpg",
+            thumbnail: "../../images/event5.jpg",
+        },
+        {
+            src: "../../images/event1.PNG",
+            thumbnail: "../../images/event1.PNG",
+        },
+        {
+            src: "../../images/event2.PNG",
+            thumbnail: "../../images/event2.PNG",
+        },
+        {
+            src: "../../images/event3.PNG",
+            thumbnail: "../../images/event3.PNG",
+        },
+        {
+            src: "../../images/event6.jpg",
+            thumbnail: "../../images/event6.jpg",
+        },
+        {
+            src: "../../images/event7.jpg",
+            thumbnail: "../../images/event7.jpg",
+        },
+        {
+            src: "../../images/event8.jpg",
+            thumbnail: "../../images/event8.jpg",
+        },
+        {
+            src: "../../images/event9.jpg",
+            thumbnail: "../../images/event9.jpg",
+        },
+    ]
 
     return (
         <div className='Event_box'>
